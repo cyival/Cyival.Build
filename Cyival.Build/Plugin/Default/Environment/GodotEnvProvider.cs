03d4aad0a26fdc6d2057ab201b3b9c4654f2d0a8
NoFun <nofuncoding@qq.com>
2025-09-21 13:32:10 +0800

    feat: godot provider for godotenv

---
+++ Cyival.Build/Plugin/Default/Environment/GodotEnvProvider.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using Cyival.Build.Environment;
+using Microsoft.Extensions.Logging;
+
+namespace Cyival.Build.Plugin.Default.Environment;
+
+public class GodotEnvProvider : IEnvironmentProvider<GodotInstance>
+{
+    private ILogger _logger = BuildApp.LoggerFactory.CreateLogger<GodotEnvProvider>();
+    
+    public bool CanProvide()
+    {
+        if (!OperatingSystem.IsWindows()) return false;
+        
+        try
+        {
+            Process.Start("godotenv", "--version");
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    
+    public IEnumerable<GodotInstance> GetEnvironment()
+    {
+        // Only for windows now.
+        
+        var appdata = System.Environment.GetEnvironmentVariable("APPDATA")
+                      ?? throw new InvalidOperationException("Cannot access the APPDATA environment variable.");
+        var basePath = Path.Combine(appdata, "godotenv", "godot", "versions"); // TODO: Support for config Godot.InstallationsPath
+        if (!Directory.Exists(basePath))
+            return [];
+
+        // should get a list of full path to executables.
+        var exeList = Directory.GetFiles(basePath, "Godot*.exe", SearchOption.AllDirectories)
+            .Where(str => !str.Contains("console")).ToList();
+
+        var instances = new List<GodotInstance>();
+        foreach (var exePath in exeList)
+        {
+            try
+            {
+                var instance = new GodotInstance(exePath);
+                instances.Add(instance);
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning("Failed to parse godot instance at {path} caused by:\n{exception}", exePath, e);
+            }
+            
+        }
+
+        return instances;
+    }
+    
+    // TODO: Support for other platforms
+}
